77f0dbb892719a5141ee2a7e2d8b9976baa3b335
joshuaPZ <joshuaprieto8@gmail.com>
2024-09-21 22:25:37 -0500

    Implementar método para agregar cursos al carrito
    Se ha añadido el método agregarCursoAlCarrito en la clase correspondiente, que permite a los estudiantes agregar un curso a su carrito de compras. Este método incluye validaciones para evitar que se agreguen cursos ya inscritos o que ya estén en el carrito, mejorando así la usabilidad del sistema.

--- src/main/java/Servicios/ServicioEstudiante.java
+++ src/main/java/Servicios/ServicioEstudiante.java
@@ -1,4 +1,5 @@
 package Servicios;
+import Entidades.Curso;
 import Entidades.Estudiante;
 
 import java.util.ArrayList;
@@ -12,4 +13,29 @@ public class ServicioEstudiante {
         this.servicioCurso = servicioCurso;
         this.estudiantes = new ArrayList<>();
     }
+
+    // Agregar un curso dado al carrito del estudiante
+    public boolean agregarCursoAlCarrito(Estudiante estudiante, Curso curso) {
+        if (estudiante != null && curso != null) {
+            List<Curso> cursosInscritos = estudiante.getCursos();
+            List<Curso> carrito = estudiante.getCarrito();
+
+            if (cursosInscritos.contains(curso)) {
+                System.out.println("El curso ya está inscrito.");
+                return false;
+            }
+
+            if (carrito.contains(curso)) {
+                System.out.println("El curso ya está en el carrito.");
+                return false;
+            }
+
+            carrito.add(curso);
+            System.out.println("Curso agregado al carrito: " + curso.getiD());
+            return true;
+        }
+        return false;
+    }
+
+
 }
