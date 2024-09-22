7287cbb00bc535c189b1bb73405a3fc6cc9197fe
Santiago Chitiva <schitiva@javeriana.edu.co>
2024-09-22 18:48:51 -0500

    Implementar método para ver el horario de un profesor
    Implementación del método verHorario en los servicios del profesor. Este método imprime el horario de los cursos asignados a un profesor, mostrando el ID del curso y los horarios correspondientes. Además, retorna la lista de cursos del profesor. Si el profesor no tiene cursos asignados o es nulo, se imprime un mensaje de error y se retorna una lista vacía.

--- src/main/java/Servicios/ServicioProfesor.java
+++ src/main/java/Servicios/ServicioProfesor.java
@@ -10,6 +10,17 @@ public class ServicioProfesor {
         this.profesores = new ArrayList<>();
     }
 
+    public List<Curso> verHorario(Profesor profesor) {
+        if (profesor != null && profesor.getCursos() != null) {
+            System.out.println("Horario del Profesor " + profesor.getNombre() + ":");
+            for (Curso curso : profesor.getCursos()) {
+                System.out.println("Curso: " + curso.getiD() + " - Horarios: " + curso.getHorarios());
+            }
+            return profesor.getCursos(); // Retorna la lista de cursos
+        } else {
+            System.out.println("El profesor no tiene cursos asignados o es nulo.");
+            return new ArrayList<>(); // Retorna una lista vacía en caso de error
 
-
+        }
+    }
 }
