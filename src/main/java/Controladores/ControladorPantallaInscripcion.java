d43a6cea29e16dc2d2de9fa51f9d12f2284b6bcb
ahenaon00 <ahenao.2005@gmail.com>
2024-10-08 10:00:20 -0500

    Pantalla de Inscripcion arreglada, ahora es responsiva. Pendiente solucionar botones de Revisar Plan de Estudios y Cambiar Carga Académica

---
+++ src/main/java/Controladores/ControladorPantallaInscripcion.java
@@ -0,0 +1,56 @@
+package Controladores;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControladorPantallaInscripcion {
+
+    @FXML
+    private AnchorPane backInscPane;
+
+    @FXML
+    private Button botonBaja;
+
+    @FXML
+    private Button botonBuscar;
+
+    @FXML
+    private Button botonInscribir;
+
+    @FXML
+    private AnchorPane buscarMateriaPane;
+
+    @FXML
+    private AnchorPane cambiarCargaPane;
+
+    @FXML
+    private AnchorPane darBajaPane;
+
+    @FXML
+    private HBox hboxTop;
+
+    @FXML
+    private Label inscText;
+
+    @FXML
+    private AnchorPane inscribirMateriaPane;
+
+    @FXML
+    private Label labelCarritoTitle;
+
+    @FXML
+    private Label labelInscritasTitle;
+
+    @FXML
+    private AnchorPane revisarPlanPane;
+
+
+}
